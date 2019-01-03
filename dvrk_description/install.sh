23f4d50b6a74d24ba49e47065a9c3504bad3b7a8
Adnan Munawar <adnan.munawar@live.com>
2019-01-03 17:03:21 -0500

    Updated install script

--- dvrk_description/install.sh
+++ dvrk_description/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 varname=y
+
+if [ ! -d ~/.gazebo1 ]; then
+	echo "~/.gazebo directory doesn't exist. Are you sure gazebo is installed?"
+fi
+
 if [ -d ~/.gazebo/models/psm ] || [ -d ~/.gazebo/models/mtm ] || [ -d ~/.gazebo/models/ecm ]\
 	|| [ -d ~/.gazebo/models/suj ] || [ -d ~/.gazebo/models/dvrk ]
 then
@@ -8,6 +13,9 @@ then
 fi 
 
 if [ $varname == "y" ]; then
+	if [ ! -d ~/.gazebo/models ]; then
+		mkdir -p ~/.gazebo/models
+	fi
  	cp -a dvrk/ ecm/ mtm/ psm/ suj/ tool_lnd/ ~/.gazebo/models
  	echo Models copied to: ~/.gazebo/models
 fi
